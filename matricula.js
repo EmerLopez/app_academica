53a6f6a6aa7458e9d46f6dc39485fb982c241b2c
Fernando-Jse <ferloto1304@gmail.com>
2024-02-19 21:29:34 -0600

    Terminacion del formulario de matricula alumnos haciendo uso de vue.js

---
+++ matricula.js
@@ -0,0 +1,194 @@
+Vue.component('componente-matriculas', {
+    data() {
+        return {
+            valor:'',
+            matriculas:[],
+            accion:'nuevo',
+            matricula:{
+                idMatricula: new Date().getTime(),
+                codigo:'',
+                nombre:'',
+                carrera:'',
+                monto:'',
+                condicion:'',
+                modalidad:'',
+                foto:'',
+            }
+        }
+    },
+    methods:{
+        buscarMatricula(e){
+            this.listar();
+        },
+        eliminarMatricula(idMatricula){
+            if( confirm(`Esta seguro de elimina el matricula?`) ){
+                let store = abrirStore('matriculas', 'readwrite'),
+                query = store.delete(idMatricula);
+            query.onsuccess = e=>{
+                this.nuevoMatricula();
+                this.listar();
+            };
+            }
+        },
+        modificarMatricula(matricula){
+            this.accion = 'modificar';
+            this.matricula = matricula;
+        },
+        guardarMatricula(){
+            //almacenamiento del objeto Matriculas en indexedDB
+            let store = abrirStore('matriculas', 'readwrite'),
+                query = store.put({...this.matricula});
+            query.onsuccess = e=>{
+                this.nuevoMatricula();
+                this.listar();
+            };
+            query.onerror = e=>{
+                console.error('Error al guardar en matriculas', e.message());
+            };
+        },
+        nuevoMatricula(){
+            this.accion = 'nuevo';
+            this.matricula = {
+                idMatricula: new Date().getTime(),
+                codigo:'',
+                nombre:'',
+                carrera:'',
+                monto: '',
+                condicion:'',
+                modalidad:''
+            }
+        },
+        listar(){
+            let store = abrirStore('matriculas', 'readonly'),
+                data = store.getAll();
+            data.onsuccess = resp=>{
+                this.matriculas = data.result
+                    .filter(matricula=>matricula.codigo.includes(this.valor) || 
+                    matricula.nombre.toLowerCase().includes(this.valor.toLowerCase()) || 
+                    matricula.carrera.toLowerCase().includes(this.valor.toLowerCase()) ||
+                    matricula.condicion.toLowerCase().includes(this.valor.toLowerCase()) ||
+                    matricula.modalidad.toLowerCase().includes(this.valor.toLowerCase()));
+            };
+
+        
+        },
+
+    },
+    template: `
+        <div class="row">
+            <div class="col col-md-6">
+                <div class="card text-bg-dark">
+                    <div class="card-header">REGISTRO DE MATRICULAS</div>
+                    <div class="catd-body">
+                        <div class="row p-1">
+                            <div class="col col-md-2">Codigo</div>
+                            <div class="col col-md-3">
+                                <input v-model="matricula.codigo" type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row p-1">
+                            <div class="col col-md-2">Nombre</div>
+                            <div class="col col-md-5">
+                                <input v-model="matricula.nombre" type="text" class="form-control">
+                            </div>
+                        </div>
+                
+                        <div class="row p-1">
+                            <div class="col col-md-2">Carrera</div>
+                            <div class="col col-md-3">
+                                <input v-model="matricula.carrera" type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row p-1">
+                            <div class="col col-md-2">Monto</div>
+                            <div class="col col-md-3">
+                                <input v-model="matricula.monto"  type="number" min="0" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row p-1">
+                            <div class="col col-md-2">Condicion</div>
+                            <div class="col col-md-3">
+                            <select v-model="matricula.condicion" class="form-control">
+                            <option value="BECA">BECA</option>
+                            <option value="Media BECA">Media BECA</option>
+                            <option value="Pagante">Pagante</option>
+                        </select>
+                            </div>
+                        </div>
+                        
+                        <div class="row p-1">
+                            <div class="col col-md-2">Modalidad</div>
+                            <div class="col col-md-3">
+                            <select v-model="matricula.modalidad" class="form-control">
+                            <option value="Presencial">Presencial</option>
+                            <option value="Semi Presencial">Semi Presencial</option>
+                            <option value="Virtual">Virtual</option>
+                            </div>
+                        </div>
+                        
+                        <div class="row p-1">
+                            <div class="col col-md-2">
+                                <img :src="matricula.foto" width="50"/>
+                            </div>
+                            <div class="col col-md-4">
+                                <div class="mb-3">
+                                    <label for="formFile" class="form-label">Seleccione la foto</label>
+                                    <input class="form-control" type="file" id="formFile" 
+                                        accept="image/*" onChange="seleccionarFoto(this)">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row p-1">
+                            <div class="col">
+                                <button @click.prevent.default="guardarMatricula" class="btn btn-success">GUARDAR</button>
+                                <button @click.prevent.default="nuevoMatricula" class="btn btn-warning">NUEVO</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-md-8">
+                <div class="card text-bg-dark">
+                    <div class="card-header">LISTADO DE MATRICULAS</div>
+                    <div class="card-body">
+                        <form id="frmMatricula">
+                            <table class="table table-dark table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>BUSCAR</th>
+                                        <th colspan="5">
+                                            <input placeholder="codigo, nombre, carrera" type="search" v-model="valor" @keyup="buscarMatricula" class="form-control">
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>CODIGO</th>
+                                        <th>NOMBRE</th>
+                                        <th>CARRERA</th>
+                                        <th>MONTO</th>
+                                        <th>CONDICION</th>
+                                        <th>MODALIDAD</th>
+                                        <th>Foto</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr @click="modificarMatricula(matricula)" v-for="matricula in matriculas" :key="matricula.idMatricula">
+                                        <td>{{matricula.codigo}}</td>
+                                        <td>{{matricula.nombre}}</td>
+                                        <td>{{matricula.carrera}}</td>
+                                        <td>{{matricula.monto}}</td>
+                                        <td>{{matricula.condicion}}</td>
+                                        <td>{{matricula.modalidad}}</td>
+                                        <td><img :src="matricula.foto" alt="Foto del producto" style="max-width: 100px; max-height: 100px;"></td>
+                                        <td><button @click.prevent.default="eliminarMatricula(matricula.idMatricula)" class="btn btn-danger">del</button></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+});
